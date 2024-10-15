deff7bea3167e329bb0322e7afca9e2f5ad00085
Roy <roy.rjacob.jacob@gmail.com>
2024-10-15 22:18:37 +1100

    final register and login designs

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,75 +1,93 @@
-import Link from "next/link";
+"use client";
 import Image from "next/image";
-import { LatestPost } from "~/app/_components/post";
-import { getServerAuthSession } from "~/server/auth";
-import { api, HydrateClient } from "~/trpc/server";
-import { text } from "stream/consumers";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { ChangeEvent, useState } from "react";
+import { FaLock, FaUser } from "react-icons/fa";
+import DetailsInput from "../_components/details-input";
+export default function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-export default async function Home() {
-  const hello = await api.post.hello({ text: "from tRPC" });
-  const session = await getServerAuthSession();
+  const router = useRouter(); // Initialize useRouter
 
-  void api.post.getLatest.prefetch();
+  const handleChange =
+    (setter: React.Dispatch<React.SetStateAction<string>>) =>
+    (event: ChangeEvent<HTMLInputElement>) => {
+      setter(event.target.value);
+    };
+
+  const handleClick = () => {
+    router.push("");
+  };
 
   return (
-    <HydrateClient>
-      <main className="grid h-screen grid-cols-2">
-        <div className="center flex h-full items-center justify-start">
-          <div className="p-16">
-            <h1 className="flex flex-row text-7xl font-semibold">
-              Task<p className="text-brand-purple-dark">Findr</p>
-            </h1>
-            <p className="pl-4 font-light">
-              We aim to create a platform exclusively for university students,
-              enabling them to post and fulfill tasks within their own
-              community, fostering collaboration, skill-sharing, and
-              affordability while ensuring trust and convenience within a known
-              network.
-            </p>
-          </div>
-        </div>
-        <div className="login-background flex h-screen flex-col items-center gap-7 p-24 pl-64">
-          <Image
-            src={"/register-email.png"}
-            alt={"register email image"}
-            width={250} // Set a default width
-            height={250} // Set a default height to maintain aspect ratio
-            // className="h-auto w-1/2 md:w-1/3 lg:w-1/4" // Responsively adjust the width
-            // layout="responsive" // Ensure the image maintains aspect ratio
-          />
-          <div className="flex flex-col justify-center text-center">
-            <h1 className="text-3xl font-semibold text-white">Register</h1>
-            <p className="text-[11px] font-extralight text-white">
-              Create your new account
-            </p>
-          </div>
-          <div className="flex w-[80%] flex-col gap-6 p-5">
-            <input
-              type="text"
-              className="rounded-lg p-[6px] text-sm shadow-2xl"
-            ></input>
-            <input
-              type="text"
-              className="rounded-lg p-[6px] text-sm shadow-2xl"
-            ></input>
-            <input
-              type="text"
-              className="rounded-lg p-[6px] text-sm shadow-2xl"
-            ></input>
-          </div>
-
-          <button className="bg-brand-purple-dark rounded-full p-2 px-12 text-white">
-            Sign up
-          </button>
-
-          <p className="flex flex-row text-xs text-white">
-            Have an account?&nbsp;
-            <Link href={"/login"} className="text-brand-purple-dark underline">
-              Sign In
-            </Link>
+    <main className="grid h-screen grid-cols-2">
+      <div className="center flex h-full items-center justify-start">
+        <div className="flex flex-col gap-3 p-16">
+          <h1 className="text-brand-purple-light flex flex-row text-7xl font-semibold">
+            Schedula
+          </h1>
+          <p className="pl-1 font-light">
+            Meet Schedula &#45; your ultimate study companion! Effortlessly
+            create a customisable study schedule based on your chosen subjects,
+            keeping you organized and on track throughout your university
+            journey. Maximize your productivity with Schedula!
           </p>
         </div>
-      </main>
-    </HydrateClient>
+      </div>
+      <div className="login-background flex h-screen flex-col items-center gap-3 p-24 pl-64">
+        <Image
+          src={"/register-email.png"}
+          alt={"register email image"}
+          width={250} // Set a default width
+          height={250} // Set a default height to maintain aspect ratio
+          // className="h-auto w-1/2 md:w-1/3 lg:w-1/4" // Responsively adjust the width
+          // layout="responsive" // Ensure the image maintains aspect ratio
+        />
+        <div className="flex flex-col justify-center text-center">
+          <h1 className="text-3xl font-semibold text-white">Log in</h1>
+          <p className="text-[11px] font-extralight text-white">
+            Log in to your account
+          </p>
+        </div>
+        <div className="flex w-full flex-col items-center justify-center">
+          <div className="flex w-[80%] flex-col gap-6 p-5 pb-2">
+            <DetailsInput
+              type="text"
+              placeholder="Username"
+              icon={<FaUser className="text-brand-purple-dark h-4 w-4" />}
+              value={username}
+              onChange={handleChange(setUsername)}
+            />
+            <DetailsInput
+              type="password"
+              placeholder="Password"
+              icon={<FaLock className="text-brand-purple-dark h-4 w-4" />}
+              value={password}
+              password={true}
+              onChange={handleChange(setPassword)}
+            />
+          </div>
+          <div className="flex w-[70%] flex-row justify-between pb-6 text-xs font-light text-white">
+            <Link href={""}>Forgot password?</Link>
+            <Link href={""}>Forgot username?</Link>
+          </div>
+        </div>
+        <button
+          onClick={handleClick}
+          className="bg-brand-purple-dark rounded-full p-2 px-12 text-white shadow-2xl"
+        >
+          Sign in
+        </button>
+
+        <p className="flex flex-row pt-7 text-xs text-white">
+          Don&apos;t have an account?&nbsp;
+          <Link href={"/register"} className="text-brand-purple-dark underline">
+            Sign Up
+          </Link>
+        </p>
+      </div>
+    </main>
   );
 }
