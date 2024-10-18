3b91821165526eb9b4d5a6c5a18c3b9bc4bd1537
Roy <roy.rjacob.jacob@gmail.com>
2024-10-18 23:25:18 +1100

    modals ongoing

--- src/app/_components/DailyPreviewDate.tsx
+++ src/app/_components/DailyPreviewDate.tsx
@@ -9,7 +9,7 @@ interface DailyPreviewDateProps {
 const DailyPreviewDate = ({ Day, Date, selected }: DailyPreviewDateProps) => {
   return (
     <div
-      className={`w-[50px] rounded-2xl p-2 text-center text-sm font-light text-white ${selected ? `bg-brand-purple-dark` : `bg-brand-purple-light`}`}
+      className={`w-[50px] cursor-pointer rounded-2xl p-2 text-center text-sm font-light text-white hover:opacity-90 ${selected ? `bg-brand-purple-dark` : `bg-brand-purple-light`}`}
     >
       <p>{Day}</p>
       <p>{Date}</p>
